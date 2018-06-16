528a3c6bb5d13d08fdd86bd08d2d1c9b58b36602
Bob <bob@bethanysciences.net>
2018-06-16 13:48:14 -0600

    mbr

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# arduino
+# Python Projects and Notes
--- plot_rand_mp.py
+++ plot_rand_mp.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import os, sys, errno
 import re
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 def plotData(outputDir, plotNum):
 	outFilename = "plot_%d.pdf" % (plotNum,)
 	outFilepath = os.path.join(outputDir, outFilename)
-	
+
 	# Plot some random data
 	# Adapted from: http://matplotlib.org/examples/shapes_and_collections/scatter_demo.html
 	N = 500
@@ -28,8 +28,8 @@ def plotData(outputDir, plotNum):
 	plt.savefig(outFilepath)
 	# Clear figure so that the next plot this worker makes will not contain
 	# data from previous plots
-	plt.clf() 
-	
+	plt.clf()
+
 	return (plotNum, outFilepath)
 
 
@@ -40,7 +40,7 @@ if __name__ == '__main__':
                         help='The directory to which plot files should be saved')
     parser.add_argument('-n', '--numPlots', required=False, type=int, default=32,
     					help='The number of plots to make')
-    parser.add_argument('--numProcessors', required=False, type=int, 
+    parser.add_argument('--numProcessors', required=False, type=int,
     					default=multiprocessing.cpu_count(),
     					help='Number of processors to use. ' + \
     					"Default for this machine is %d" % (multiprocessing.cpu_count(),) )
@@ -48,13 +48,13 @@ if __name__ == '__main__':
 
     if not os.path.isdir(args.outputDir) or not os.access(args.outputDir, os.W_OK):
     	sys.exit("Unable to write to output directory %s" % (args.outputDir,) )
-    
+
     if args.numPlots < 1:
     	sys.exit('Number of plots must be greater than 0')
-    
+
     if args.numProcessors < 1:
     	sys.exit('Number of processors to use must be greater than 0')
-    
+
     # Start my pool
     pool = multiprocessing.Pool( args.numProcessors )
 
@@ -67,7 +67,7 @@ if __name__ == '__main__':
     while plotNum < args.numPlots:
     	plotNum += 1
     	tasks.append( (args.outputDir, plotNum, ) )
-    
+
     # Run tasks
     results = [pool.apply_async( plotData, t ) for t in tasks]
 
